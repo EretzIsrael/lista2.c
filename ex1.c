7a091bd0f5878b9ab7c7c50532d72a6052fb3cad
EretzIsrael <eretzisrael2022@gmail.com>
2023-08-17 22:27:44 -0300

    tentei

---
+++ ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    float p;
+    int f;
+    printf (" Digite o preco do produto: ");
+    scanf ("%d", &p);
+     f = p * 0.08;
+        if (p > 1000);
+            printf (" O preco final = %d", f);
+
+        {
+            /* code */
+        }
+        
+    return 0;
+}
